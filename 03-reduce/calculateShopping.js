f6e95f5abdf17587c656214237f80cab69196ca4
Adriana Garcia <adrianagarciarodriguezzz@gmail.com>
2024-01-22 18:59:47 +0100

    Add calculateShopping function and test

---
+++ 03-reduce/calculateShopping.js
@@ -0,0 +1,18 @@
+/**
+ * Given 'WISHES' array, define function 'calculateShopping' to return how much it would cost to just buy everything at once.
+ *
+ * expected: 227005
+ */
+
+const WISHES = [
+  { title: "Tesla Model S", price: 90000 },
+  { title: "4 carat diamond ring", price: 45000 },
+  { title: "Fancy hacky Sack", price: 5 },
+  { title: "Gold fidgit spinner", price: 2000 },
+  { title: "A second Tesla Model S", price: 90000 },
+];
+
+export const calculateShopping = (wishes) => {
+  const totalcost = wishes.reduce((acc, wish) => acc + wish.price, 0);
+  return totalcost;
+};
---
+++ 03-reduce/calculateShopping.test.js
@@ -0,0 +1,19 @@
+import { calculateShopping } from "./calculateShopping";
+
+describe("calculateShopping", () => {
+  test("return how much it would cost to just buy everything at once", () => {
+    //Arrange
+    const WISHES = [
+      { title: "Tesla Model S", price: 90000 },
+      { title: "4 carat diamond ring", price: 45000 },
+      { title: "Fancy hacky Sack", price: 5 },
+      { title: "Gold fidgit spinner", price: 2000 },
+      { title: "A second Tesla Model S", price: 90000 },
+    ];
+    const expected = 227005;
+    //Act
+    const result = calculateShopping(WISHES);
+    //Assert
+    expect(result).toEqual(expected);
+  });
+});
